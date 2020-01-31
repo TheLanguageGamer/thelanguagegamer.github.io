f6d145d450169545bf8a65e908b9e1d55cb4db4f
TheLanguageGamer <justin.m.helms+1@gmail.com>
2020-01-30 20:31:14 -0800

    article pages

--- generator.py
+++ generator.py
@@ -30,7 +30,8 @@ def createArticlePreview(text):
 
 class Article(object):
 
-	def __init__(self, text):
+	def __init__(self, text, filename):
+		self.filename = filename
 		self.html = BeautifulSoup(text, "html.parser")
 
 		dates = self.html.find_all("div", class_="date")
@@ -39,11 +40,43 @@ class Article(object):
 		else:
 			self.date = ""
 
+		self.text = self.html.prettify()
+
+	def asPage(self):
+		copy = BeautifulSoup(self.text, "html.parser")
+		titles = self.html.find_all("div", class_="title")
+		if (len(titles) > 0):
+			title = titles[0]
+			string = title.string
+			title.string = ""
+			a = copy.new_tag("a", href=os.path.join("articles/", self.filename))
+			a.string = string
+			title.append(a)
+		else:
+			assert(False)
+		return self.html.prettify()
+
+
 	def preview(self):
+		copy = BeautifulSoup(self.text, "html.parser")
 		bodies = self.html.find_all("div", class_="article")
 		if (len(bodies) > 0):
 			bodies[0].decompose()
+		else:
+			assert(False)
 		return self.html.prettify()
+
+def outputArticle(article):
+
+	if not os.path.exists('articles'):
+		os.makedirs('articles')
+
+	articleTemplate = open(os.path.join("content/", "article.html"), 'r').read()
+	articleTemplate = addContent("article", article.asPage(), articleTemplate)
+
+	articleOutput = open(os.path.join("articles/", article.filename), 'w')
+	articleOutput.write(articleTemplate)
+	articleOutput.close()
 
 
 def generateArticlesContent():
@@ -56,13 +89,14 @@ def generateArticlesContent():
 	for filename in os.listdir(path):
 		if filename.endswith(".html"):
 			content = open(os.path.join(path, filename), 'r').read()
-			article = Article(content)
+			article = Article(content, filename)
 			articles.append(article)
 
 	articles.sort(key=lambda a : a.date)
 	articles.reverse()
 
 	for article in articles:
+		outputArticle(article)
 		ret += '	<div class="article">\n'
 		ret += article.preview()
 		ret += '\n	</div>'
